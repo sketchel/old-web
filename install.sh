49e8a4e51afc5481d325d37ee959d28985b02f84
javascript man genius <56984694+kashima-dot-com@users.noreply.github.com>
2019-11-17 19:17:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 # Btw, this will probably only work using Ubuntu.
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
-npm install
-node index.js
+npm i
+npm i -g nodemon
+nodemon index.js
