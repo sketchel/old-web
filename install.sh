3b1b01fcb34cadcd1a05734023cd66e8bf1031b6
blizzy <34592028+blizma@users.noreply.github.com>
2019-11-18 18:49:28 -0500

    :rocket: New version published.

--- install.sh
+++ install.sh
@@ -4,8 +4,4 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm i
 npm i -g nodemon
-npm run css-build
-npm run dark-build
-npm run css-watch
-npm run dark-watch
 nodemon index.js
