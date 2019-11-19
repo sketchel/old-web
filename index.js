00be0a6a729b7dd968d932b13dc3fd4cdf1ef856
javascript man genius <56984694+kashima-dot-com@users.noreply.github.com>
2019-11-18 21:40:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -169,14 +169,14 @@ app.get('/api/v1/get-user/:userId', (req, res) => {
     if (!user) {
       res.status(404)
     } else {
+      user = req.params.userId
       var bio = users.get(`${user}.bio`)
       var avatar = users.get(`${user}.avatar`)
       var rank = users.get(`${user}.rank`)
       var following = users.get(`${user}.following`)
       var followers = users.get(`${user}.followers`)
       var joindate = users.get(`${user}.joindate`)
-      var dark = users.get(`${user}.dark`)
-      res.json({ bio:bio, avatar:avatar, rank:rank, following:following, followers:followers, joindate:joindate, dark:dark })
+      res.json({ bio:bio, avatar:avatar, rank:rank, following:following, followers:followers, joindate:joindate })
       res.status(200)
     }
   }
