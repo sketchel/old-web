0c299329e9ca2817f3e4c992a016b8b9965bc68d
xMinota <jpichardo369@gmail.com>
2020-09-27 19:55:09 -0400

    aaaaaaaaaaa

--- public/js/autolink.js
+++ public/js/autolink.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   var str = document.getElementById('bio').innerText
   const button = (url) => { return `<a href="${url}" title="Be careful when clicking on unknown links.">${url}</a>` }
 
-  window.original = str;
+  window.original = str
   const r = /(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi
   str = str.replace(r, (matched, index, original) => {
     matched = matched.split(' ').join('');
